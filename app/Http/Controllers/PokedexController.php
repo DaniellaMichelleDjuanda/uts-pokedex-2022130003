22b267bf2c6fdf61fa90c4e776b91d510673bc7e
DaniellaMichelleDjuanda <lalamdj28@gmail.com>
2024-10-11 14:31:45 +0700

    uts-pbwl

---
+++ app/Http/Controllers/PokedexController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use illuminate\Http\Request;
+use App\Models\Pokemon;
+
+class PokedexController extends Controller
+{
+    public function __invoke(Request $request)
+    {
+        $Pokemon = Pokemon::paginate(15);
+
+        return view('main', compact('Pokemon'));
+    }
+}
